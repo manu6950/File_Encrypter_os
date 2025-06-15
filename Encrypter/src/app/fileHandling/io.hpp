89365544beb0e43aae525702cee24565a1c7c6f7
manu6950 <mail.connect.pr@gmail.com>
2025-06-16 00:35:38 +0530

    Add files via upload

---
+++ Encrypter/src/app/fileHandling/io.hpp
@@ -0,0 +1,19 @@
+// IO.hpp
+#ifndef IO_HPP
+#define IO_HPP
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+class IO {
+public:
+    IO(const std::string& file_path);
+    ~IO();
+    std::fstream getFileStream();
+
+private:
+    std::fstream file_stream;
+};
+
+#endif
